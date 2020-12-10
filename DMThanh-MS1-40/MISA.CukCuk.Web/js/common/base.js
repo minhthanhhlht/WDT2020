7dfbbb7adfa8a46b578712f9229353b9a753f509
minhthanhhlht <minhthanhhlht@gmail.com>
2020-12-10 14:57:49 +0700

    Tạo lớp con BaseJS

--- DMThanh-MS1-40/MISA.CukCuk.Web/js/common/base.js
+++ DMThanh-MS1-40/MISA.CukCuk.Web/js/common/base.js
@@ -1,1 +1,104 @@
-﻿
+﻿$(document).ready(function () {
+     //dialog
+
+    dialog = $(".customer-dialog").dialog({
+        autoOpen: false,
+        height: 600,
+        width: 700,
+        modal: true,
+        });
+})
+
+class BaseJS {
+    constructor() {
+        this.loadData();
+        this.Event();
+        
+    }
+
+    /**
+* Load dữ liệu và append vào table
+* */
+    loadData() {
+        $.ajax({
+            url: 'http://api.manhnv.net/api/customers',
+            method: 'GET',
+        }).done(function (response) {
+            console.log(response);
+            //3. 
+            $('#tbList tbody').empty();
+            for (var i = 0; i < response.length; i++) {
+                console.log(response[i]);
+                var DateOfB = Dateformat(response[i].DateOfBirth)
+                var trHtml = `<tr class="line1">
+                        <td>${response[i].CustomerCode}</td>
+                        <td>${response[i].FullName}</td>
+                        <td>${response[i].GenderName}</td>
+                        <td>${DateOfB}</td>
+                        <td>${response[i].CustomerGroupName}</td>
+                        <td>${response[i].PhoneNumber}</td>
+                        <td>${response[i].Email}</td>
+                        <td style="max-width:250px" title="${response[i].Address}"><span style="width:100px">${response[i].Address}</span></td>
+                        <td>${response[i].DebitAmount || ""}</td>
+                        <td>${response[i].MemberCardCode}</td>
+                    </tr>`;
+                $('#tbList >tbody:last-child').append(trHtml);
+            }
+        }).fail(function (response) {
+
+        })
+    }
+    S
+
+    /**
+    * Gán các sự kiện
+    * */
+    Event() {
+        // Gán các sự kiện:
+        $('#btnAdd').click(function () {
+            dialog.dialog('open');
+
+        })
+        $('#btnCancel').click(function () {
+            dialog.dialog('close');
+        })
+        $('#tbList').on('dblclick', 'tr', function () {
+            // load dữ liệu chi tiết:
+
+            // Hiển thị dialog thông tin chi tiết:
+            dialog.dialog('open');
+        })
+    }
+
+    /**
+* Thêm dữ liệu
+* */
+    addData() {
+
+    }
+
+
+    /**
+    * Sửa dữ liệu
+    * */
+    editData() {
+
+    }
+
+    /**
+    * Xoá dữ liệu
+    * */
+    deleteData() {
+
+    }
+
+}
+
+
+
+
+
+
+
+}
+
--- DMThanh-MS1-40/MISA.CukCuk.Web/js/page/customers.js
+++ DMThanh-MS1-40/MISA.CukCuk.Web/js/page/customers.js
@@ -1,105 +1,27 @@
 ﻿$(document).ready(function () {
-    //dialog
-    dialog = $(".customer-dialog").dialog({
-        autoOpen: false,
-        height: 600,
-        width: 700,
-        modal: true,
-       
-    });
-    new CustomerJS();
-   Event();
+   
+       new CustomerJS();
+   
     
    
 })
 
-class CustomerJS {
+class CustomerJS extends BaseJS {
     constructor() {
-        this.loadData();
-    }
-/**
-* Load dữ liệu và append vào table
-* */
-    loadData() {
-        $.ajax({
-            url: 'http://api.manhnv.net/api/customers',
-            method: 'GET',
-        }).done(function (response) {
-            console.log(response);
-            //3. 
-            $('#tbList tbody').empty();
-            for (var i = 0; i < response.length; i++) {
-                console.log(response[i]);
-                var DateOfB = Dateformat(response[i].DateOfBirth)
-                var trHtml = `<tr class="line1">
-                        <td>${response[i].CustomerCode}</td>
-                        <td>${response[i].FullName}</td>
-                        <td>${response[i].GenderName}</td>
-                        <td>${DateOfB}</td>
-                        <td>${response[i].CustomerGroupName}</td>
-                        <td>${response[i].PhoneNumber}</td>
-                        <td>${response[i].Email}</td>
-                        <td style="max-width:250px" title="${response[i].Address}"><span style="width:100px">${response[i].Address}</span></td>
-                        <td>${response[i].DebitAmount || ""}</td>
-                        <td>${response[i].MemberCardCode}</td>
-                    </tr>`;
-                $('#tbList >tbody:last-child').append(trHtml);
-            }
-        }).fail(function (response) {
-
-        })
+        super();
+        super.Event();
+        
     }
 
-    /**
-* Thêm dữ liệu
-* */
-    addData() {
-
-    }
-
-
-/**
-* Sửa dữ liệu
-* */
-    editData() {
-
-    }
-
-/**
-* Xoá dữ liệu
-* */
-    deleteData() {
-
-    }
-
-}
-
-/**
- * Gán các sự kiện 
- * */
-function Event() {
-    // Gán các sự kiện:
-    $('#btnAdd').click(function () {
-        dialog.dialog('open');
-    })
-
-    $('#btnCancel').click(function () {
-        dialog.dialog('close');
-    })
-    $('#tbList').on('dblclick', 'tr', function () {
-        // load dữ liệu chi tiết:
-
-        // Hiển thị dialog thông tin chi tiết:
-        dialog.dialog('open');
-    })
-}
 
 
 
 /**
- * Format dữ liệu sang kiểu dd/mm/yyyy
- * @param {any} date
- */
+* Format dữ liệu sang kiểu dd/mm/yyyy
+* @param {any} date
+*/
+
+
 
 function Dateformat(date) {
     var date = new Date(date);
@@ -107,9 +29,13 @@ function Dateformat(date) {
     var day = date.getDate().toString();
     day = day.length > 1 ? day : '0' + day;
     //month
-        var month = (1 + date.getMonth()).toString();
+    var month = (1 + date.getMonth()).toString();
     month = month.length > 1 ? month : '0' + month;
-        //year
+    //year
     var year = date.getFullYear();
     return day + '/' + month + '/' + year;
 }
+
+
+
+
